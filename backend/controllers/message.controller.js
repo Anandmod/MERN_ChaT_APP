10322eddb792a45c61d2917aa891d439307ae477
Anandmod <128707163+Anandmod@users.noreply.github.com>
2024-11-24 03:32:16 +0530

    4th part completed socket

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,10 +1,12 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
+
 export const sendMessage = async (req, res) => {
     //console.log("sent mess",req.param.id);
     try{
         const { message } = req.body;
-		const { id:receivedId} = req.params;
+		const { id:receiverId} = req.params;
 		const senderId = req.user._id;
         let conversation = await Conversation.findOne({
 			participants: { $all: [senderId, receiverId] },
@@ -31,6 +33,12 @@ export const sendMessage = async (req, res) => {
 
 		// this will run in parallel
 		await Promise.all([conversation.save(), newMessage.save()]);
+		const receiverSocketId = getReceiverSocketId(receiverId);
+		if (receiverSocketId) {
+			// io.to(<socket_id>).emit() used to send events to specific client
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
+
 		res.status(201).json(newMessage);
 
     }catch (error) {
